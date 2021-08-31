5a9fd08546ccc9bb0fc962519b2db82b55ec1fd7
James <jamesrandell@me.com>
2021-08-31 14:50:41 +0100

    getting tsc to work in local package format

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 export {};
 let test: string  = 'Hello worldd';
 console.log(test);
-alert("esee");
+alert("eseweeee");
 
 
 
@@ -13,3 +13,17 @@ Http.send();
 Http.onreadystatechange=(e)=>{
 console.log(Http.responseText)
 }
+
+function greeter(person) {
+    return "Hello, " + person;
+  }
+   
+  let user = "Jane User";
+   
+  document.body.textContent = greeter(user);
+
+  function setBackgroundColor() {
+    document.body.style.backgroundColor = "rgb(255,0,0)";
+}
+
+setBackgroundColor();
--- resource/js/main.js
+++ resource/js/main.js
@@ -1,23 +1,20 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var test = 'Hello world';
-    console.log(test);
-    var special = document.querySelectorAll("p");
-    special.innerText('fff');
-    var Http = new XMLHttpRequest();
-    var url = 'index.html';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = function (e) {
-        console.log(Http.responseText);
-    };
-});
+let test = 'Hello worldd';
+console.log(test);
+alert("eseweeee");
+const Http = new XMLHttpRequest();
+const url = 'index.html';
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+};
+function greeter(person) {
+    return "Hello, " + person;
+}
+let user = "Jane User";
+document.body.textContent = greeter(user);
+function setBackgroundColor() {
+    document.body.style.backgroundColor = "rgb(255,0,0)";
+}
+setBackgroundColor();
+export {};
