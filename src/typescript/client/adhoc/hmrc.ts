4a519a8d5fd07fd480c70686283cab59b12949cb
James Randell <jamesrandell@me.com>
2024-03-24 18:30:10 +0000

    new dev

---
+++ src/typescript/client/adhoc/hmrc.ts
@@ -0,0 +1,27 @@
+export default class hmrc {
+    constructor() {
+        const that = this0
+    }
+    
+    public login() {
+
+    
+    
+    let url = 'https://test-www.tax.service.gov.uk/oauth/authorize?'
+      + 'response_type=code'
+      + '&client_id=VooJtM2gsbwDjEmSHIci4WEab657'
+      + '&scope=read:vat'
+      + '&state=state1'
+      + '&redirect_uri=https://digitaltalc.com'
+    //  + '&code_challenge=[CODE-CHALLENGE]'
+    //  + '&code_challenge_method=[CODE-CHALLENGE-METHOD]'
+    window.location.href = url;
+    //console.log(url)
+    //var response = UrlFetchApp.fetch(url, options);
+    //Logger.log(response);
+    
+   //   let url = 'https://test-api.service.hmrc.gov.uk/organisations/vat/460042827/obligations';
+    //var response = fetch(url, options);
+    //Logger.log(response);
+    }
+}
